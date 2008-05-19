e61b95c3b7bafc4e08c50ff55853fc1458688dcc
Marcel Haerry <haerry@puzzle.ch>
2008-05-19 15:23:53 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# rubygems module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "rubygems": }
+class rubygems {}
