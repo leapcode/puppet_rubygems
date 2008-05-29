813fc9259925ff6a6c806486bda942a402c31562
Simon Josi <sjosi@pw-sjosi.puzzle.itc>
2008-05-29 18:39:42 +0200

    added puzzle copyright notice

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,17 @@
-#######################################
+#
 # rubygems module
 # original by luke kanies
 # http://github.com/lak
-# adapted by  Puzzle ITC 
-# haerry+puppet(at)puzzle.ch
-#######################################
+#
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 class rubygems {
 	package { rubygems: ensure => installed }
