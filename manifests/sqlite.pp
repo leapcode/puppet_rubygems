737a6cb947861419ad7a9566077adda56e67d57c
mh <mh@immerda.ch>
2009-05-29 02:54:54 +0200

    finally added rubygems sqlite

--- manifests/sqlite.pp
+++ manifests/sqlite.pp
@@ -1,1 +1,9 @@
-class ru
+class rubygems::sqlite {
+  include rubygems
+  include sqlite::devel
+  package{'sqlite-ruby':
+    ensure => present,
+    provider => gem,
+    require => [ Package['rubygems'], Pakcage['sqlite-devel'] ],
+  }
+}
