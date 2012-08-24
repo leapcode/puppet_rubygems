91a5718ef915d2482d4f31b60ba5d154e18463ec
mh <mh@immerda.ch>
2012-08-24 18:32:35 +0200

    from centos 6 on this can be installed that way

--- manifests/postgres.pp
+++ manifests/postgres.pp
@@ -1,4 +1,10 @@
 class rubygems::postgres {
+  if $operatingsystem == 'CentOS' and $lsbmajdistrelease > 5 {
+    package{'rubygem-pg':
+      ensure => installed,
+    }
+  } else {
     require postgres::devel
     rubygems::gem{'ruby-pg':}
+  }
 }
