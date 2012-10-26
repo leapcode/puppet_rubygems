66e0ff081aa7fb9bbb043f81635fb15b8f5c09b4
varac <varacanero@zeromail.org>
2012-10-26 18:18:03 +0200

    highline: debian/ubuntu package named ruby-highline

--- manifests/highline.pp
+++ manifests/highline.pp
@@ -3,4 +3,11 @@ class rubygems::highline {
   package{'rubygem-highline':
     ensure => present,
   }
+
+  case $::operatingsystem {
+    debian,ubuntu: {
+      Package['rubygem-highline']{
+        name => 'ruby-highline'
+    }
+  }
 }
