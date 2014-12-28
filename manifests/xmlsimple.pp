0df73b6128184faca333561ea67c7d05701a1d79
mh <mh@immerda.ch>
2014-12-28 13:16:10 +0100

    linting

--- manifests/xmlsimple.pp
+++ manifests/xmlsimple.pp
@@ -3,7 +3,7 @@ class rubygems::xmlsimple {
   package{'rubygem-xml-simple':
     ensure => present,
   }
-  case $operatingsystem {
+  case $::operatingsystem {
     debian,ubuntu: {
       Package['rubygem-xml-simple']{
         name => 'libxml-simple-ruby'
